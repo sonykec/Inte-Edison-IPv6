8844ce022291d247bd26f10be559f93e171496b8
sonykec <ivan.ontaneda@hotmail.com>
2017-01-05 11:10:31 -0500

    Add files via upload

--- bin/install_ffmpeg.sh
+++ bin/install_ffmpeg.sh
@@ -1,24 +1,20 @@
-#!/bin/sh
+# Con este archivo se descarga ffmpeg. Si el archivo a descargarse no es la ultima version, se puede visitar la siguiente pagina para bajar la ultima version http://johnvansickle.com/ffmpeg/.
 
-# If the download doesn't work, the release link may have changed.
-# Check http://johnvansickle.com/ffmpeg/, copy the address of the
-# latest release, and replace in the shell script.
-
-# CHANGE THIS TO THE APPROPRIATE FILENAME
+# Cambiar aqui si el archivo no se descarga
 FNAME=ffmpeg-release-32bit-static.tar.xz
 
-echo "Creating ~/bin directory if it doesn't exist..."
+echo "Creando el directorio ~/bin si no existe aun..."
 mkdir -p ~/bin
 
-echo "Removing old versions of ffmpeg..."
+echo "Removiendo versiones obsoletas de ffmpeg..."
 rm -rf ~/bin/ffmpeg*
 
-echo "Downloading ffmpeg..."
+echo "Descargando ffmpeg..."
 wget -P /home/root/bin http://johnvansickle.com/ffmpeg/releases/$FNAME
 
-echo "Unpacking..."
+echo "Descomprimiendo..."
 tar -xf /home/root/bin/$FNAME -C /home/root/bin
 
-echo "Cleaning up..."
+echo "Limpiando Archivos..."
 rm /home/root/bin/$FNAME
 mv /home/root/bin/ffmpeg*/ /home/root/bin/ffmpeg
